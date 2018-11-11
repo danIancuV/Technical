d3d8e2f137c71a90cfb41481149de9952fd8b605
danIancuV <33359205+danIancuV@users.noreply.github.com>
2018-11-11 21:28:27 +0200

    additional re factoring separate back-end with all dependencies exposed

---
+++ MutablevsImmutable/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MutablevsImmutable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //----------------------imutable
+
+            string a = "Wirtek";
+            string b = a;
+            bool areEqual = Object.ReferenceEquals(a, b);
+            Console.WriteLine("Strings are equal: {0}", areEqual);
+
+            a = "Viking";
+            bool areEqualafterModif = Object.ReferenceEquals(a, b);
+            Console.WriteLine("Strings are equal: {0}", areEqualafterModif);
+
+            //---------------------------mutable
+
+            StringBuilder stra = new StringBuilder();
+            stra.Append("Wirtek");
+
+            StringBuilder strb = new StringBuilder();
+            strb = stra;
+            bool strAreEqual = Object.ReferenceEquals(stra, strb);
+            Console.WriteLine("Strings are equal: {0}", strAreEqual);
+
+            stra.Append("viking");
+            bool strAreEqualafterModif = Object.ReferenceEquals(stra, strb);
+            Console.WriteLine("Strings are equal: {0}", strAreEqualafterModif);
+
+            //-------------------------------imutable
+
+            int x = 1;
+            int y = x;
+            bool areEqualInts = x.Equals(y);
+            Console.WriteLine("Ints are equal: {0}", areEqualInts);
+
+            x = 2;
+            bool areEqualIntsafterModif = x.Equals(y);
+            Console.WriteLine("Ints are equal: {0}", areEqualIntsafterModif);
+
+
+            //-------------------------------mutable   
+
+            var firstarr = new[] {1};
+            var secondarr = firstarr;
+            bool arrAreEqual = Object.ReferenceEquals(firstarr, secondarr);
+            Console.WriteLine("Arrays are equal: {0}", arrAreEqual);
+
+            //firstarr= new[] {2};
+            firstarr[0] = 2;
+            bool arrAreEqualafterModif = Object.ReferenceEquals(firstarr, secondarr);
+            Console.WriteLine("Arrays are equal: {0}", arrAreEqualafterModif);
+        }
+    }
+}
---
+++ refparam/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefParam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mystring = "Wirtek";
+            Console.WriteLine("Inside Main, before calling the change method, the string value is: {0}", mystring);            
+            ModifyString(ref mystring);
+            Console.WriteLine("Inside Main, after calling the change method, the string value is: {0}", mystring);
+            Console.ReadKey();
+
+            char[] mychararray = new char[] {'a', 'b', 'c'};
+            Console.WriteLine("Inside Main, before calling the change method, the array first element is: {0}", mychararray[0]);
+            ModifyCharArray(ref mychararray);
+            Console.WriteLine("Inside Main, after calling the change method, the array first element is: {0}", mychararray[0]);
+        }
+
+        static void ModifyString(ref string modifstring)
+        {
+            modifstring = "viking";
+            Console.WriteLine("Inside the change method the string value is: {0}", modifstring);           
+        }
+
+        static void ModifyCharArray(ref char[] modifarray)
+        {
+            modifarray[0] = '1';
+            modifarray = new char[] { 'd', 'e', 'f' };           
+            Console.WriteLine("Inside the change method, the array first element is: {0}", modifarray[0]);          
+        }
+    }
+}
+
